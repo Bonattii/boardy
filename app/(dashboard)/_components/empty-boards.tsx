6d5ac70abd7b90158e85579adedd0e57969a21d7
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2024-01-31 15:59:52 -0800

    Create convex mutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,7 +1,33 @@
-import { Button } from '@/components/ui/button'
+'use client'
+
+import { toast } from 'sonner'
 import Image from 'next/image'
+import { useOrganization } from '@clerk/nextjs'
+
+import { api } from '@/convex/_generated/api'
+import { Button } from '@/components/ui/button'
+import { useApiMutation } from '@/hooks/use-api-mutation'
 
 export function EmptyBoards() {
+  const { organization } = useOrganization()
+  const { mutate, pending } = useApiMutation(api.board.create)
+
+  const onClick = () => {
+    if (!organization) {
+      return
+    }
+
+    mutate({
+      title: 'Untitled',
+      orgId: organization.id
+    })
+      .then((id) => {
+        toast.success('Board created!')
+        // TODO: Redirect to board/{id}
+      })
+      .catch(() => toast.error('Failed to create board'))
+  }
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/note.svg" alt="Empty Search" height={110} width={110} />
@@ -13,7 +39,9 @@ export function EmptyBoards() {
       </p>
 
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button size="lg" onClick={onClick} disabled={pending}>
+          Create board
+        </Button>
       </div>
     </div>
   )
