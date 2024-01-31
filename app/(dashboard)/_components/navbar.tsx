fe671a2ddaaaa7b7c4fac5fb23742175e9aea8ea
Rodrigo Bonatti <bonatti@Rodrigos-MacBook-Pro.local>
2024-01-31 09:10:51 -0800

    Create dashboard layout

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { UserButton } from '@clerk/nextjs'
+
+export function Navbar() {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex flex-1">{/* TODO: Add Search */}</div>
+
+      <UserButton />
+    </div>
+  )
+}
